77c3be377308494e3cd9831aa4ac405741737a56
romain.savidan <romain.savidan@cdiscount.com>
2017-12-08 14:31:57 +0100

    Added skeleton for unit tests

---
+++ test/test_mcts.py
@@ -0,0 +1,28 @@
+import unittest
+
+
+class TestMCTSMethods(unittest.TestCase):
+
+    def test_select(self):
+        # TODO
+        pass
+
+    def test_expand(self):
+        # TODO
+        pass
+
+    def test_simulate(self):
+        # TODO
+        pass
+
+    def test_backpropagate(self):
+        # TODO
+        pass
+
+    def test_predict(self):
+        # TODO
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ test/test_tictactoe.py
@@ -0,0 +1,28 @@
+import unittest
+
+
+class TestTicTacToeMethods(unittest.TestCase):
+
+    def test_display(self):
+        # TODO
+        pass
+
+    def test_legal_plays(self):
+        # TODO
+        pass
+
+    def test_play(self):
+        # TODO
+        pass
+
+    def test_winner(self):
+        # TODO
+        pass
+
+    def test_game(self):
+        # TODO
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
