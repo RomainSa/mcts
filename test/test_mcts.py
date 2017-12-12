73294ab890fc3a72fa36c0af8db8132d90f263ad
romain.savidan <romain.savidan@cdiscount.com>
2017-12-12 15:32:44 +0100

    Updated MCTS unit tests

--- test/test_mcts.py
+++ test/test_mcts.py
@@ -1,27 +1,141 @@
 import unittest
+from copy import deepcopy
+
+from anytree import Node, PreOrderIter
+from anytree.search import findall
+
+from mcts import MonteCarloTreeSearch
+from games.tictactoe import Game
 
 
 class TestMCTSMethods(unittest.TestCase):
 
-    def test_select(self):
-        # TODO
-        pass
+    def make_level(self, starting_node, n_nodes):
+        possible_plays = starting_node.game.legal_plays()
+        for i in range(n_nodes):
+            new_game = deepcopy(starting_node.game)
+            move = possible_plays.pop()
+            new_game.play(move)
+            new_name = starting_node.name + '_' + str(i)
+            _ = Node(name=new_name, parent=starting_node, game=new_game, **self.tree.node_init_params)
+
+    def update_nodes(self, n_plays, n_wins, n_ties=0, nodes=None, node_names=None, include_ancestors=False):
+        # get selection as nodes
+        selected_nodes = []
+        if node_names is not None and nodes is None:
+            for node_name in node_names:
+                for node in findall(self.tree.root, filter_=lambda n: node_name == n.name):
+                    selected_nodes.append(node)
+        elif node_names is None and nodes is not None:
+            selected_nodes = nodes
+        else:
+            raise ValueError('One of node_names and nodes arguments must be None')
+
+        # get ancestors if needed
+        nodes_to_update = []
+        if include_ancestors:
+            for node in selected_nodes:
+                for ancestor in node.ancestors:
+                    nodes_to_update.append(ancestor)
+                nodes_to_update.append(node)
+        else:
+            for node in selected_nodes:
+                nodes_to_update.append(node)
+
+        # update corresponding nodes
+        for node in nodes_to_update:
+            node.n_plays = n_plays
+            node.n_wins = n_wins
+            node.n_ties = n_ties
+
+    def setUp(self):
+        # tree init
+        self.game = Game()
+        self.tree = MonteCarloTreeSearch(game=self.game)
+        # 1st level (complete)
+        self.make_level(self.tree.root, 9)
+        # 2nd level (complete)
+        self.make_level(self.tree.root.children[3], 8)
+        # 3rd level (incomplete)
+        self.make_level(self.tree.root.children[3].children[4], 3)
+
+    def test_select_level1(self):
+        # create fake wins and plays
+        self.update_nodes(nodes=list(PreOrderIter(self.tree.root)), n_plays=1000, n_wins=2)
+
+        # give best attributes to one node
+        node_name = '0_6'
+        self.update_nodes(node_names=[node_name], n_plays=50, n_wins=45)
+
+        selected_node = self.tree.select()   # to update scores
+        self.assertEqual(node_name, selected_node.name)
+
+    def test_select_level2(self):
+        # create fake wins and plays
+        self.update_nodes(nodes=list(PreOrderIter(self.tree.root)), n_plays=1000, n_wins=2)
+
+        # give best attributes to ancestors of one node
+        node_name = '0_3_6'
+        self.update_nodes(node_names=[node_name], include_ancestors=True, n_plays=50, n_wins=45)
+
+        selected_node = self.tree.select()   # to update scores
+        self.assertEqual(node_name, selected_node.name)
+
+    def test_select_level3(self):
+        # create fake wins and plays
+        self.update_nodes(nodes=list(PreOrderIter(self.tree.root)), n_plays=1000, n_wins=2)
+
+        # give best attributes to ancestors of one node
+        node_name = '0_3_4_1'
+        node = findall(self.tree.root, filter_=lambda n: n.name == node_name)[0]
+        self.update_nodes(node_names=[node_name], include_ancestors=True, n_plays=50, n_wins=45)
+
+        selected_node = self.tree.select()   # to update scores
+        self.assertEqual(node.parent.name, selected_node.name)   # because node is not fully expanded
 
     def test_expand(self):
-        # TODO
-        pass
+        # expand one node
+        node = self.tree.root.children[3].children[4]
+        n_nodes_before_expand = len(list(PreOrderIter(self.tree.root)))
+        self.tree.expand(node)
+        n_nodes_after_expand = len(list(PreOrderIter(self.tree.root)))
+
+        self.assertEqual(n_nodes_before_expand+1, n_nodes_after_expand)
 
     def test_simulate(self):
-        # TODO
-        pass
+        n_wins, _ = self.tree.simulate(node=self.tree.root, n_simulations=100)
+        self.assertGreater(n_wins, 1)
 
     def test_backpropagate(self):
-        # TODO
-        pass
+        # select a node from with backpropagation start
+        node_name = '0_3_4_1'
+        node = findall(self.tree.root, filter_=lambda n: n.name == node_name)[0]
+        total_plays_before = sum([n.n_plays for n in PreOrderIter(self.tree.root)])
 
-    def test_predict(self):
-        # TODO
-        pass
+        # perform backpropagation
+        self.tree.backpropagate(node, n_plays=100, n_wins=10, n_ties=0)
+        total_plays_after = sum([n.n_plays for n in PreOrderIter(self.tree.root)])
+        total_wins_after = sum([n.n_wins for n in PreOrderIter(self.tree.root)])
+
+        self.assertEquals(total_plays_after-total_plays_before, 4*100)
+        self.assertGreater(total_wins_after, 1)
+
+    def test_recommend(self):
+        # create fake wins and plays
+        self.update_nodes(nodes=list(PreOrderIter(self.tree.root)), n_plays=100, n_wins=1)
+
+        # backpropagate good attributes to node where same player than root has to play
+        node_name = '0_3_4'
+        node = findall(self.tree.root, filter_=lambda n: node_name == n.name)[0]
+        self.tree.backpropagate(node, n_plays=1000, n_wins=999, n_ties=0)
+
+        # corresponding level1 name
+        node_to_recommend_name = '_'.join(node_name.split('_')[:2])
+        node_to_recommend = findall(self.tree.root, filter_=lambda n: node_to_recommend_name == n.name)[0]
+
+        # move corresponding to node
+        recommended_move = self.tree.recommended_play()
+        self.assertEqual(node_to_recommend.game.last_play, recommended_move)
 
 
 if __name__ == '__main__':
