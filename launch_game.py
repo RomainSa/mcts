81f58a0f6d69109d2c80c8fcf60f8bfc81aa37cb
romain.savidan <romain.savidan@cdiscount.com>
2017-12-13 19:27:20 +0100

    Updated default values for MCTS search

--- launch_game.py
+++ launch_game.py
@@ -13,13 +13,14 @@ def main():
     Run an interactive game with MCTS advice
     """
     logging.basicConfig(level=logging.CRITICAL)
-    game = Game(board_size=3)
+    game = Game()
     # print init version of game
     game.show_board()
     while game.legal_plays():
         # run Monte Carlo Tree Search and show recommended move
         tree = MonteCarloTreeSearch(game=game)
-        tree.search(max_iterations=1000, max_runtime=4, n_simulations=1)
+        tree.search(max_iterations=10000, max_runtime=3, n_simulations=1)
+        tree.show_tree(level=1)
         print("MCTS recommends: {}".format(tree.recommended_play()))
         # ask user for move to play and play it
         move = tuple([int(s) for s in input("Move to play (format: .,.) : ").split(',')])
@@ -33,9 +34,9 @@ def main():
         game.show_board()
     if game.winner() is None:
         print("It's a tie! :|")
-    elif game.winner().value == 1:
+    elif game.winner() == game.players[0]:
         print("You win! :)")
-    elif game.winner().value == -1:
+    elif game.winner() == game.players[1]:
         print("You lose! :(")
     else:
         raise ValueError('Unknown game status')
